b4f777b6b7461b6d3bf634bcb2a9b74c4747c73f
Łukasz Mrozek <morskk@gmail.com>
2019-04-02 18:41:55 +0200

    Update default settings

--- Mors.AppPlatform.Service/Properties/Settings.Designer.cs
+++ Mors.AppPlatform.Service/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Mors.AppPlatform.Service.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.0.1.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.0.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,7 +25,7 @@ namespace Mors.AppPlatform.Service.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("../data/events.txt")]
+        [global::System.Configuration.DefaultSettingValueAttribute("..\\..\\..\\data\\events.txt")]
         public string EventFilePath {
             get {
                 return ((string)(this["EventFilePath"]));
@@ -52,7 +52,7 @@ namespace Mors.AppPlatform.Service.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("..\\sites")]
+        [global::System.Configuration.DefaultSettingValueAttribute("..\\..\\..\\content\\sites")]
         public string SitesPath {
             get {
                 return ((string)(this["SitesPath"]));
