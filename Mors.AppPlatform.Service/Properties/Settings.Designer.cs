06be260c7e9bdcddb8177c1b95302053391b08e5
Łukasz Mrozek <morskk@gmail.com>
2022-11-28 19:36:52 +0100

    Adjust URL reservation user

--- Mors.AppPlatform.Service/Properties/Settings.Designer.cs
+++ Mors.AppPlatform.Service/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Mors.AppPlatform.Service.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -43,7 +43,7 @@ namespace Mors.AppPlatform.Service.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("Mors")]
+        [global::System.Configuration.DefaultSettingValueAttribute("morsk")]
         public string UrlReservationUser {
             get {
                 return ((string)(this["UrlReservationUser"]));
